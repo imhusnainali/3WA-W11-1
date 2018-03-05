b3bc22f5c04ba9157beb462d2f0a0a40550c50e3
lkatkus <laimonas@katkus.eu>
2018-03-05 11:58:48 +0200

    Basic migrations

---
+++ database/migrations/2018_03_05_090422_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->nullable()->unsigned();
+            $table->integer('reservation_table')->nullable()->unsigned();
+            $table->dateTime('reservation_time');
+            $table->boolean('confirmed');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
---
+++ database/migrations/2018_03_05_093752_create_carts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('token', 255);
+            $table->integer('client_id')->nullable()->unsigned();
+            $table->integer('order_id')->nullable()->unsigned();
+            $table->integer('dish_id')->nullable()->unsigned();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
